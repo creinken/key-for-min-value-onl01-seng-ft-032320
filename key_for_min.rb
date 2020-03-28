94e8960897ea27185985c0ca2950d04bd41a8642
Christopher Reinken <christopherreinken@gmail.com>
2020-03-28 22:53:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  comparator = []
+  return_key = ""
+  while name_hash.length > 1
+    name_hash.each do |key, value|
+      name_hash.length.times do |x|
+        comparator << (value <=> name_hash[x])
+      end
+      if comparator.any?(-1)
+        name_hash.delete(key)
+      end
+    end
+    return_key == "" ? nil : return_key
+  end
 end
